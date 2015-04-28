068f2866abf939a58332b9bc9f34e467f8942685
SBC4 <SBC4@SBC03>
2015-04-28 18:08:24 +0700

    tambah&kurang = souce code

--- src/addition.java
+++ src/addition.java
@@ -8,5 +8,14 @@
  * @author SBC4
  */
 public class addition {
+    public static void main(String[] args) {  
+    int a, b, hasil;  
+    a = 1;  
+    b= 2;  
+    hasil = a+ b;  
+   System.out.println("Hasil Penjumlahan Bilangan = "+hasil);  
+    //System.out.println(hasil);  
+    }  
+
     
 }
